760bd758f90ae16f39e6781cb82266b9502d83d9
icepicklife <hanzdrive01@gmail.com>
2025-02-27 19:06:17 +0800

    added admin features for bonus points

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = [RecipeIngredientInline]
+
+admin.site.register(Recipe, RecipeAdmin)
 
 # Register your models here.
